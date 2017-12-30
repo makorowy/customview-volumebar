2b224d7e27f9451b3860c7c13dbc783ced05aa6c
Makor <mateusz.budzar@droidsonroids.pl>
2017-12-30 17:00:25 +0100

    Add drawing a circle and react on volume level changes

--- app/src/main/java/com/example/customview/MainActivity.kt
+++ app/src/main/java/com/example/customview/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.example.customview
 
+import android.content.Context
+import android.media.AudioManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.KeyEvent
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var audioManager: AudioManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+
+        volumeBar.calibrateVolumeLevels(
+                audioManager.getStreamMaxVolume(AudioManager.STREAM_SYSTEM),
+                audioManager.getStreamVolume(AudioManager.STREAM_SYSTEM))
+    }
+
+    override fun dispatchKeyEvent(event: KeyEvent?): Boolean {
+        super.dispatchKeyEvent(event)
+        if (event?.keyCode == KeyEvent.KEYCODE_VOLUME_UP ||
+                event?.keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            volumeBar.setVolumeLevel(audioManager.getStreamVolume(AudioManager.STREAM_SYSTEM))
+        }
+        return false
     }
 }
--- app/src/main/java/com/example/customview/VolumeBarView.kt
+++ app/src/main/java/com/example/customview/VolumeBarView.kt
@@ -2,15 +2,20 @@ package com.example.customview
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
 class VolumeBarView : View {
 
-    private var linePaint: Paint? = null
-    private var defaultLineWidth: Int? = null
-    private var defaultLineHeight: Int? = null
+    private var barPaint: Paint? = null
+    private var thumbPaint: Paint? = null
+    private var defaultBarWidth: Int? = null
+    private var defaultBarHeight: Int? = null
+
+    private var volumeLevelWidth: Int? = null
+    private var currentVolumeLevel: Int? = null
 
     constructor(context: Context) : super(context) {
         init()
@@ -21,9 +26,14 @@ class VolumeBarView : View {
     }
 
     private fun init() {
-        linePaint = Paint()
-        defaultLineWidth = resources.getDimensionPixelSize(R.dimen.volume_bar_default_width)
-        defaultLineHeight = resources.getDimensionPixelSize(R.dimen.volume_bar_default_height)
+        barPaint = Paint()
+        barPaint?.color = Color.MAGENTA
+
+        thumbPaint = Paint()
+        thumbPaint?.color = Color.GREEN
+
+        defaultBarWidth = resources.getDimensionPixelSize(R.dimen.volume_bar_default_width)
+        defaultBarHeight = resources.getDimensionPixelSize(R.dimen.volume_bar_default_height)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -35,23 +45,58 @@ class VolumeBarView : View {
 
         val width = when (widthMode) {
             View.MeasureSpec.EXACTLY -> widthSize
-            View.MeasureSpec.AT_MOST -> defaultLineWidth
-            View.MeasureSpec.UNSPECIFIED -> defaultLineWidth
-            else -> defaultLineWidth
+            View.MeasureSpec.AT_MOST -> defaultBarWidth
+            View.MeasureSpec.UNSPECIFIED -> defaultBarWidth
+            else -> defaultBarWidth
         }
 
         val height = when (heightMode) {
             View.MeasureSpec.EXACTLY -> heightSize
-            View.MeasureSpec.AT_MOST -> defaultLineHeight
-            View.MeasureSpec.UNSPECIFIED -> defaultLineHeight
-            else -> defaultLineHeight
+            View.MeasureSpec.AT_MOST -> defaultBarHeight
+            View.MeasureSpec.UNSPECIFIED -> defaultBarHeight
+            else -> defaultBarHeight
         }
 
         setMeasuredDimension(width!!, height!!)
     }
 
     override fun onDraw(canvas: Canvas?) {
-        canvas?.drawLine(0.0F, 0.0F, width.toFloat(), 0.0f, linePaint)
+        drawBar(canvas)
+        drawThumb(canvas)
+    }
+
+    private fun drawBar(canvas: Canvas?) {
+        canvas?.drawRect(0.0F, 0.0F, width.toFloat(), height.toFloat(), barPaint)
+    }
+
+    private fun drawThumb(canvas: Canvas?) {
+        val thumbX = calculateThumbX()
+        val thumbY = height.toFloat() / 2.0F
+        val radius = height.toFloat() / 2
+
+        canvas?.drawCircle(thumbX, thumbY, radius, thumbPaint)
+    }
+
+    private fun calculateThumbX(): Float {
+        val volumeLevelWidth = this.volumeLevelWidth
+        val currentVolumeLevel = this.currentVolumeLevel
+
+        return if (volumeLevelWidth != null && currentVolumeLevel != null) {
+            (width / volumeLevelWidth * currentVolumeLevel).toFloat()
+        } else {
+            0.0F
+        }
+    }
+
+    fun calibrateVolumeLevels(volumeLevelsCount: Int, currentVolumeLevel: Int) {
+        this.volumeLevelWidth = volumeLevelsCount
+        this.currentVolumeLevel = currentVolumeLevel
+        invalidate()
+    }
+
+    fun setVolumeLevel(volumeLevel: Int) {
+        currentVolumeLevel = volumeLevel
+        invalidate()
     }
 
 }
