10f0711ee48f6f2a152ebf2fb2000af318f2084b
Makor <mateusz.budzar@droidsonroids.pl>
2017-12-30 12:18:56 +0100

    Create a VolumeBarView class

---
+++ app/src/main/java/pl/droidsonroids/customviewexample/VolumeBarView.kt
@@ -0,0 +1,14 @@
+package pl.droidsonroids.customviewexample
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class VolumeBarView : View {
+
+    constructor(context: Context) : super(context) {}
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {}
+
+}
+
